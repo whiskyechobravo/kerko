5a53649ac251a881c9a0098debdba27d757999af
David Lesieur <david@davidlesieur.com>
2019-11-19 10:41:55 -0500

    Upgrade default PopperJS version.

--- kerko/__init__.py
+++ kerko/__init__.py
@@ -72,7 +72,7 @@ def init_default_config(state):
 
     state.app.config.setdefault('KERKO_BOOTSTRAP_VERSION', '4.3.1')
     state.app.config.setdefault('KERKO_JQUERY_VERSION', '3.4.1')
-    state.app.config.setdefault('KERKO_POPPER_VERSION', '1.14.7')
+    state.app.config.setdefault('KERKO_POPPER_VERSION', '1.16.0')
     state.app.config.setdefault('KERKO_WITH_JQUERY', True)
     state.app.config.setdefault('KERKO_WITH_POPPER', True)
 
