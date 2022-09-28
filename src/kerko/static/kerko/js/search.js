b2186e2a815d0906249a1e1c15d56318348e3b9d
David Lesieur <david@davidlesieur.com>
2022-09-28 17:25:41 -0400

    Scroll on facet collapse to keep facet into view.

--- src/kerko/static/kerko/js/search.js
+++ src/kerko/static/kerko/js/search.js
@@ -13,12 +13,32 @@ jQuery(function($) {
   });
 
   $('.collapse').on('show.bs.collapse', function(e) {
+    // On expanding, remove continuation hint.
     e.stopPropagation();
     $(e.currentTarget).prev('.continued').removeClass('continued-hint');
   });
   $('.collapse').on('hidden.bs.collapse', function(e) {
+    // After collapsing, add continuation hint.
     e.stopPropagation();
     $(e.currentTarget).prev('.continued').addClass('continued-hint');
   });
-  // TODO: Autoscroll page to keep facet in view when collapsing on "view less".
+  $('.collapse').on('hide.bs.collapse', function(e) {
+    // Prevent event from bubbling up to parent '.collapse' elements. Without
+    // this, the handler for '.continued + .collapse' can get called because of
+    // a collapse event triggered by child element.
+    e.stopPropagation();
+  });
+  if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+    $('.continued + .collapse').on('hide.bs.collapse', function(e) {
+      // On collapsing, scroll up if the continued element's bottom is not visible.
+      e.stopPropagation();
+      var $el = $(e.currentTarget).prev('.continued');
+      var wantedVisiblePosition = $el.offset().top + $el.outerHeight() - 100;
+      if ($(document).scrollTop() > wantedVisiblePosition) {
+        // Bootstrap's standard collapse duration is 350ms. Any different duration
+        // for the scrolling would feel unpleasant to the user.
+        $('html, body').animate({scrollTop: Math.max(0, wantedVisiblePosition)}, 350);
+      }
+    });
+  }
 });
