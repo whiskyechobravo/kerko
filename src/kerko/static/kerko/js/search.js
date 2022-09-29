543b21c5e67dc2c7e3604624c3403ccd98e0bd96
David Lesieur <david@davidlesieur.com>
2022-09-29 13:16:44 -0400

    Move facets HTML to modal instead of cloning.
    Avoids ending up with duplicate ids. Also prevents issues with attaching event handlers.

--- src/kerko/static/kerko/js/search.js
+++ src/kerko/static/kerko/js/search.js
@@ -1,31 +1,30 @@
 jQuery(function($) {
-  function injectModalBody(source, target) {
-    if (!$(target).hasClass('modal-body-ready')) {
-      $(target).addClass('modal-body-ready');
-      $(source).clone().prependTo($(target + ' .modal-body'));
-    }
-  }
-
+  // Handlers for facets modal.
   $('#facets-modal-toggle').click(function(e) {
-    var target = $(this).data('target');
-    injectModalBody('.facets', target);
-    $(target).modal();
+    // Move facets to modal body.
+    $('#facets').prependTo($('#facets-modal-body'));
+    $('#facets-modal').modal();
+  });
+  $('#facets-modal').on('hidden.bs.modal', function(e) {
+    // Move facets back to their normal container.
+    $('#facets').appendTo($('#facets-container'));
   });
 
+  // Handlers for collapsing behavior.
   $('.collapse').on('show.bs.collapse', function(e) {
-    // On expanding, remove continuation hint.
+    // On expanding, if prev is '.continued', remove its continuation hint.
     e.stopPropagation();
     $(e.currentTarget).prev('.continued').removeClass('continued-hint');
   });
   $('.collapse').on('hidden.bs.collapse', function(e) {
-    // After collapsing, add continuation hint.
+    // After collapsing, if prev is '.continued', put back its continuation hint.
     e.stopPropagation();
     $(e.currentTarget).prev('.continued').addClass('continued-hint');
   });
   $('.collapse').on('hide.bs.collapse', function(e) {
     // Prevent event from bubbling up to parent '.collapse' elements. Without
     // this, the handler for '.continued + .collapse' can get called because of
-    // a collapse event triggered by child element.
+    // a collapse event triggered by collapsing child element.
     e.stopPropagation();
   });
   if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
