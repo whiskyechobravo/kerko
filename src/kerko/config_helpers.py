a0d2e487aff592628aef327b290775be3445ca3e
David Lesieur <david@davidlesieur.com>
2023-06-15 09:49:47 -0400

    Rename arg for consistency with Flask API.

--- src/kerko/config_helpers.py
+++ src/kerko/config_helpers.py
@@ -345,13 +345,13 @@ class KerkoModel(BaseModel):
     relations: Dict[ElementIdStr, RelationsModel]
 
 
-def load_toml(filename: Union[str, pathlib.Path], silent=True) -> Dict[str, Any]:
+def load_toml(filename: Union[str, pathlib.Path], verbose=False) -> Dict[str, Any]:
     """Load the content of a TOML file."""
     try:
         with open(filename, 'rb') as file:
             config = tomllib.load(file)
-            if not silent:
-                print(f"Found configuration file: {filename}")
+            if verbose:
+                print(f" * Loading configuration file {filename}")
             return config
     except OSError as e:
         raise RuntimeError(f"Unable to open TOML file.\n{e}") from e
